c49483fa28eaecf85294ffa961365856f565c05a
francisco7silva <1222631@isep.ipp.pt>
2023-03-23 19:19:16 +0000

    koko

--- src/lab010.ts
+++ src/lab010.ts
@@ -1,11 +1,8 @@
-export function expression(x: number, y: number): number {
-    const z = 10;
-
+export function expression(x: number): number {
 // calcula o valor da expressão
 const resultado = Math.pow(x, 2) - 3 * x + 1;
 
 // exibe o resultado
-console.log(`O resultado da expressão x² - 3x + 1 para x = ${x} é: ${resultado}`);
-
-    return Math.pow(x, y);
+return resultado;    
 }
+console.log (expression(2));

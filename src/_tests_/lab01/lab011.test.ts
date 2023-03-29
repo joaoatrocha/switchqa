65a602f7a3faaf49e9bc2b63de85a402bbd2d903
joaoatrocha <1222635@isep.ipp.pt>
2023-03-29 19:46:38 +0100

    2903

--- src/_tests_/lab01/lab011.test.ts
+++ src/_tests_/lab01/lab011.test.ts
@@ -1,8 +1,14 @@
 import { calcularAlturaEdificio } from "../../lab01/lab011";
 
 describe("calcularAlturaEdificio", () => {	
-    it("Deve retornar a altura do edificio", () => {
-        const result = calcularAlturaEdificio(2, 9.8);
-        expect(result).toBe(19.6);
+    test("calcularAlturaEdificio(2, 9.8) = 19.6", () => {
+        //Arrange 
+        const tempoQueda = 2;
+        const aceleracaoGravidade = 9.8;
+        const expected = 19.6;
+        //Act
+        const result = calcularAlturaEdificio(tempoQueda, aceleracaoGravidade);
+        //Assert
+        expect(result).toBe(expected);
     });
-});
+})
